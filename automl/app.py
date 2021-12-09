a5c791316f8fe1442ab24b8227cabeef560e04c7
Ivan <i.fedulov@reo.ru>
2021-12-09 10:52:44 +0300

    create dirs for log and models

--- automl/app.py
+++ automl/app.py
@@ -10,17 +10,22 @@ from classification import ClassificationInputModel, do_autoclassification, NotS
 
 app = Flask(__name__)
 
+error_logs_path = 'error_logs'
+os.makedirs(error_logs_path, exist_ok=True)
+
+saved_models_path = 'saved_models'
+os.makedirs(saved_models_path, exist_ok=True)
 
 def check_calculation_status_decorator(funciton):
     def wrapper(*args, **kwargs):
         model_id = request.args.get('model_id')
 
-        if f'{model_id}.log' in os.listdir('error_logs'):
-            with open(os.path.join('error_logs', f'{model_id}.log'), 'r') as file:
+        if f'{model_id}.log' in os.listdir(error_logs_path):
+            with open(os.path.join(error_logs_path, f'{model_id}.log'), 'r') as file:
                 exception = file.readline()
             return exception, 500
 
-        elif f'{model_id}.pickle' not in os.listdir('saved_models'):
+        elif f'{model_id}.pickle' not in os.listdir(saved_models_path):
             return 'not calculated yet', 102
 
         else:
@@ -46,7 +51,7 @@ def auto_classification():
 def get_model():
     model_id = request.args.get('model_id')
     
-    with open(os.path.join('saved_models', f'{model_id}.pickle'), 'rb') as file:
+    with open(os.path.join(saved_models_path, f'{model_id}.pickle'), 'rb') as file:
         model = pickle.load(file)
     return send_file(io.BytesIO(model._to_pickle_string()), download_name='model.pickle'), 200
 
@@ -55,7 +60,7 @@ def get_model():
 @check_calculation_status_decorator
 def get_score():
     model_id = request.args.get('model_id')
-    with open(os.path.join('saved_models', f'{model_id}.pickle'), 'rb') as file:
+    with open(os.path.join(saved_models_path, f'{model_id}.pickle'), 'rb') as file:
         model = pickle.load(file)
 
     print(model.score)
